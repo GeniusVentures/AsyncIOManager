3339e1257d7bd6cacb966387a257d8df91e2e056
itsafuu <sariru@gmail.com>
2025-08-21 18:20:09 -0400

    Return buffer result as an outcome with failure states to check

--- include/FileSaver.hpp
+++ include/FileSaver.hpp
@@ -10,9 +10,10 @@ using namespace std;
 
 class FileSaver {
 public:
+    using ResultType = outcome::result<std::shared_ptr<std::pair<std::vector<std::string>, std::vector<std::vector<char>>>>>;
     virtual ~FileSaver() {}
     virtual void SaveFile(std::string filename, shared_ptr<void> data) = 0;
-    virtual void SaveASync(std::shared_ptr<boost::asio::io_context> ioc, std::function<void(std::shared_ptr<boost::asio::io_context> ioc)> handle_write, std::string filename, std::shared_ptr<std::pair<std::vector<std::string>, std::vector<std::vector<char>>>> data, std::string suffix) = 0;
+    virtual void SaveASync(std::shared_ptr<boost::asio::io_context> ioc, std::function<void(std::shared_ptr<boost::asio::io_context> ioc)> handle_write, std::string filename, ResultType data, std::string suffix) = 0;
 };
 
 #endif
--- src/HTTPCommon.cpp
+++ src/HTTPCommon.cpp
@@ -3,6 +3,26 @@
  */
 #include "HTTPCommon.hpp"
 
+OUTCOME_CPP_DEFINE_CATEGORY_3(sgns, HTTPDevice::Error, e)
+{
+    switch (e)
+    {
+    case sgns::HTTPDevice::Error::COULD_NOT_RESOLVE:
+        return "HTTP Could not resolve address";
+    case sgns::HTTPDevice::Error::HANDSHAKE_ERROR:
+        return "HTTP Handshake Error";
+    case sgns::HTTPDevice::Error::CONNECT_ERROR:
+        return "HTTP Connect Error";
+    case sgns::HTTPDevice::Error::CON_INTERRUPT:
+        return "HTTP Read Error. Connection interrupted.";   
+    case sgns::HTTPDevice::Error::NO_HEADER:
+        return "HTTP Data Read failed. No header.";
+    case sgns::HTTPDevice::Error::REQ_FAILED:
+        return "HTTP Data Read failed. Get Request Fail.";
+    }
+    return "Unknown error";
+}
+
 namespace sgns
 {
     using namespace boost::asio;
@@ -19,7 +39,7 @@ namespace sgns
         save_ = save;
     }
 
-    void HTTPDevice::StartHTTPDownload(std::shared_ptr<boost::asio::io_context> ioc, CompletionCallback handle_read, StatusCallback status)
+    void HTTPDevice::StartHTTPDownload(std::shared_ptr<boost::asio::io_context> ioc, CompletionCallback handle_read)
     {
         //Get DNS result for hostname
 
@@ -32,15 +52,18 @@ namespace sgns
         }
         catch (const boost::system::system_error& e) {
             std::cerr << "Error resolving address: " << e.what() << std::endl;
-            status(CustomResult(sgns::AsyncError::outcome::failure("HTTP Could not resolve address")));
+            handle_read(ioc, outcome::failure(Error::COULD_NOT_RESOLVE), false, false);
+            return;
         }
         catch (const std::exception& e) {
             std::cerr << "Exception: " << e.what() << std::endl;
-            status(CustomResult(sgns::AsyncError::outcome::failure("HTTP Could not resolve address")));
+            handle_read(ioc, outcome::failure(Error::COULD_NOT_RESOLVE), false, false);
+            return;
         }
         catch (...) {
             std::cerr << "Unknown error occurred during address resolution." << std::endl;
-            status(CustomResult(sgns::AsyncError::outcome::failure("HTTP Could not resolve address")));
+            handle_read(ioc, outcome::failure(Error::COULD_NOT_RESOLVE), false, false);
+            return;
         }
         //boost::asio::ip::tcp::endpoint endpoint = *results.begin();
 
@@ -59,55 +82,46 @@ namespace sgns
         if (!SSL_set_tlsext_host_name(socket->native_handle(), http_host_.c_str())) {
             unsigned long err = ERR_get_error();
             throw std::runtime_error("Failed to set SNI: " + std::string(ERR_reason_error_string(err)));
-            status(CustomResult(sgns::AsyncError::outcome::failure("Could not set SNI")));
         }
         
         //Connect socket
-        status(CustomResult(sgns::AsyncError::outcome::success(Success{ "Starting HTTP Connection" })));
-        socket->lowest_layer().async_connect(endpoint, [self = shared_from_this(), ioc, socket, handle_read, status](const boost::system::error_code& connect_error)
+        socket->lowest_layer().async_connect(endpoint, [self = shared_from_this(), ioc, socket, handle_read](const boost::system::error_code& connect_error)
             {
                 if (!connect_error)
                 {
-                    status(CustomResult(sgns::AsyncError::outcome::success(Success{ "SSL Handshake Started" })));
-                    socket->async_handshake(boost::asio::ssl::stream_base::client, [self , ioc, socket, handle_read, status](const boost::system::error_code& handshake_error) {
+                    socket->async_handshake(boost::asio::ssl::stream_base::client, [self , ioc, socket, handle_read](const boost::system::error_code& handshake_error) {
                         if (!handshake_error) {
                             // Start the asynchronous download for a specific path
-                            self->StartHTTPGet(ioc, socket, handle_read, status);
+                            self->StartHTTPGet(ioc, socket, handle_read);
                         }
                         else {
                             std::cerr << "Handshake error: " << handshake_error.message() << std::endl;
-                            status(CustomResult(sgns::AsyncError::outcome::failure("HTTP Handshake Error")));
-                            handle_read(ioc, std::shared_ptr<std::pair<std::vector<std::string>, std::vector<std::vector<char>>>>(), false, false);
+                            handle_read(ioc, outcome::failure(Error::HANDSHAKE_ERROR), false, false);
                         }
                         });
                 }
                 else {
                     std::cerr << "Connection error: " << connect_error.message() << std::endl;
-                    status(CustomResult(sgns::AsyncError::outcome::failure("HTTP Connection Error")));
-                    handle_read(ioc, std::shared_ptr<std::pair<std::vector<std::string>, std::vector<std::vector<char>>>>(), false, false);
+                    handle_read(ioc, outcome::failure(Error::CONNECT_ERROR), false, false);
                 }
             });
     }
 
     void HTTPDevice::StartHTTPGet(std::shared_ptr<boost::asio::io_context> ioc,
         std::shared_ptr<boost::asio::ssl::stream<boost::asio::ip::tcp::socket>> socket,
-        CompletionCallback handle_read,
-        StatusCallback status)
+        CompletionCallback handle_read)
     {
         //Create HTTP Get request and write to server
-        status(CustomResult(sgns::AsyncError::outcome::success(Success{ "Starting HTTP Get Request" })));
         std::string get_request = "GET " + http_path_ + " HTTP/1.1\r\nHost: " + http_host_ + "\r\nConnection: close\r\n\r\n";
-        boost::asio::async_write(*socket, boost::asio::buffer(get_request), [self = shared_from_this(), ioc, handle_read, status, socket](const boost::system::error_code& write_error, std::size_t) {
+        boost::asio::async_write(*socket, boost::asio::buffer(get_request), [self = shared_from_this(), ioc, handle_read, socket](const boost::system::error_code& write_error, std::size_t) {
             if (!write_error) {
                 //Create a buffer for returned data and read from server
                 auto headerbuff = std::make_shared<boost::asio::streambuf>();
-                status(CustomResult(sgns::AsyncError::outcome::success(Success{ "Starting HTTP File Read" })));
-                boost::asio::async_read(*socket, *headerbuff, boost::asio::transfer_all(), [self, ioc, handle_read, status, headerbuff, socket](const boost::system::error_code& read_error, std::size_t bytes_transferred) {
+                boost::asio::async_read(*socket, *headerbuff, boost::asio::transfer_all(), [self, ioc, handle_read, headerbuff, socket](const boost::system::error_code& read_error, std::size_t bytes_transferred) {
                     // Check if read completed normally with EOF (boost::asio::error::eof)
                     if (read_error && read_error != boost::asio::error::eof) {
                         // Connection was interrupted before completion
-                        status(CustomResult(sgns::AsyncError::outcome::failure("HTTP Data Read failed. Connection interrupted.")));
-                        handle_read(ioc, std::shared_ptr<std::pair<std::vector<std::string>, std::vector<std::vector<char>>>>(), false, false);
+                        handle_read(ioc, outcome::failure(Error::CON_INTERRUPT), false, false);
                         return;
                     }
                     //Make a vector buffer from data
@@ -126,7 +140,6 @@ namespace sgns
 
                         //Send this to handler to be processed.
                         //std::cout << "HTTPS Finish" << std::endl;
-                        status(CustomResult(sgns::AsyncError::outcome::success(Success{ "HTTP Get finished" })));
                         auto finaldata = std::make_shared<std::pair<std::vector<std::string>, std::vector<std::vector<char>>>>();
                         std::filesystem::path p(self->http_path_);
                         finaldata->first.push_back(p.filename().string());
@@ -138,15 +151,13 @@ namespace sgns
                         handle_read(ioc, finaldata, self->parse_, self->save_);
                     }
                     else {
-                        status(CustomResult(sgns::AsyncError::outcome::failure("HTTP Data Read failed. No header.")));
-                        handle_read(ioc, std::shared_ptr<std::pair<std::vector<std::string>, std::vector<std::vector<char>>>>(), false, false);
+                        handle_read(ioc, outcome::failure(Error::NO_HEADER), false, false);
                     }
                     });
             }
             else {
                 std::cerr << "Error in async_write: " << write_error.message() << std::endl;
-                status(CustomResult(sgns::AsyncError::outcome::failure("HTTP Data Read failed. Get Request Fail.")));
-                handle_read(ioc, std::shared_ptr<std::pair<std::vector<std::string>, std::vector<std::vector<char>>>>(), false, false);
+                handle_read(ioc, outcome::failure(Error::REQ_FAILED), false, false);
             }
             });
     }
