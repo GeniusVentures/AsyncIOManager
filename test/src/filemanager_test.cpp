c12750664797da666fe872ac17f82b0110434f97
itsafuu <sariru@gmail.com>
2026-09-04 18:59:06 -0400

    test(04-02): neutralize mnn:// prefix test to foo:// (D-32)

--- test/src/filemanager_test.cpp
+++ test/src/filemanager_test.cpp
@@ -121,7 +121,7 @@ TEST_F( FileManagerIntegrationTest, SaveASync_UnregisteredPrefixThrows )
         std::range_error );
 }
 
-TEST_F( FileManagerIntegrationTest, SaveASync_MnnPrefixThrows )
+TEST_F( FileManagerIntegrationTest, SaveASync_FooPrefixThrows )
 {
     IOContextRunner runner;
     auto            data = makeSingleFileResult( "test.bin", "content" );
@@ -129,7 +129,7 @@ TEST_F( FileManagerIntegrationTest, SaveASync_MnnPrefixThrows )
     EXPECT_THROW(
         {
             FileManager::GetInstance().SaveASync(
-                "mnn://some/path", data, runner.ioc(), []( FileManager::ResultType ) {} );
+                "foo://some/path", data, runner.ioc(), []( FileManager::ResultType ) {} );
         },
         std::range_error );
 }
